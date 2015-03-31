4f5117c609d8bbffaacd236594c3a297830be3da
Athena Yao <athena@fastmail.fm>
2015-03-31 11:50:13 -0700

    Enable static file serving from Heroku

--- plogs/wsgi.py
+++ plogs/wsgi.py
@@ -11,5 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plogs.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
