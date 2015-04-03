55da538ceab146285a243ca0ee66e7b87a3c60ac
Athena Yao <athena@fastmail.fm>
2015-04-02 18:29:51 -0700

    Proof of concept image uploads

--- plogs/buildlogs/views.py
+++ plogs/buildlogs/views.py
@@ -4,6 +4,9 @@ from django.views.generic import DetailView
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import HttpResponse
+from hashlib import sha1
+import time, os, json, base64, hmac, urllib
 from .models import BuildLog, Category, Partner, Project
 
 class BuildLogBase(FormMixin):
@@ -56,3 +59,42 @@ class BuildLogDetail(DetailView):
 
         return project_queryset
 
+@login_required
+def photo_upload_url(request):
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+
+    if 's3_object_type' not in request.GET:
+        return HttpResponse(json.dumps({}))
+
+    mime_type = request.GET['s3_object_type']
+
+    # FIXME: project from request
+    # FIXME: buildlog number from request
+    # FIXME: do actually need a better unique for this, but this will do for testing
+    project = Project.objects.latest_for_user(request.user)
+    object_name = "%s/%s/build/%s" % (request.user.id, project.id, time.time())
+
+    expires = long(time.time()+60)
+    amz_headers = ["x-amz-acl:public-read"]
+
+    md5 = ""
+    put_request = "\n".join(["PUT",
+                             "%s" % md5,
+                             mime_type,
+                             str(expires),
+                             "\n".join(amz_headers),
+                             "/%s/%s" % (S3_BUCKET, object_name),
+                            ])
+
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
+    signature = urllib.quote_plus(signature.strip())
+
+    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+
+    response = json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+         'url': url
+      })
+    return HttpResponse(response)
