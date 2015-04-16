923a60a678914c15c42c0cd26cdb477c8827b2e6
Athena Yao <athena@fastmail.fm>
2015-04-16 13:16:55 -0700

    Move project.factory.js -> active-project.factory.js
    Accidentally duplicated then edited the wrong file, consolidate into one
    for less confusion

--- plogs/buildlogs/src/js/index/active-project.js
+++ plogs/buildlogs/src/js/index/active-project.js
@@ -1,24 +1,16 @@
-var app = angular.module('index.project.factory', []);
-app.factory('ActiveProjectFactory', function ProjectFactory($q, $http) {
+var app = angular.module('index.active-project.factory', []);
+app.factory('ActiveProjectFactory', function ActiveProjectFactory($q, $http) {
     var exports = {};
 
-    function getJson(url) {
+    exports.getProject = function () {
         var deferred = $q.defer();
-        $http.get(url)
+        $http.get('/api/projects/active')
             .then(function (response) {
                 deferred.resolve(response.data);
             }, function (response) {
                 deferred.reject(response.data);
             });
         return deferred.promise;
-    }
-
-    exports.getProject = function () {
-        return getJson('/static/project-info.json');
-    };
-
-    exports.getStats = function () {
-        return getJson('/static/project.json');
     };
 
     return exports;
