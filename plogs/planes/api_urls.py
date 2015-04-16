ab8147083a6e1a7a9d9deac052b047bfd676550a
Athena Yao <athena@fastmail.fm>
2015-04-16 11:54:53 -0700

    Implement our own NestedDefaultRouter
    * uses the NestedRouterMixin provided by drf-extensions
    
    * stops using ExtendedDefaultRouter -- buggy with the new drf code
    (in particular, list_route was being treated like detail_route)

--- plogs/planes/api_urls.py
+++ plogs/planes/api_urls.py
@@ -1,8 +1,8 @@
-from rest_framework_extensions.routers import ExtendedDefaultRouter
 from plogs.main.api_views import UserViewSet
+from plogs.main.routers import NestedDefaultRouter
 from . import api_views as views
 
-router = ExtendedDefaultRouter(trailing_slash=False)
+router = NestedDefaultRouter(trailing_slash=False)
 (
     router.register(r'people', UserViewSet)
           .register(r'planes', views.UserPlaneViewSet,
