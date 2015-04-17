f2dd99a17d5777e14ccc39f696681aa0361bd2a1
Athena Yao <athena@fastmail.fm>
2015-04-16 22:55:55 -0700

    Basic pagination for buildlogs
    Eventually we'll want something more sophisticated, but this will do for
    now

--- plogs/buildlogs/src/js/buildlogs/buildlogs.controller.js
+++ plogs/buildlogs/src/js/buildlogs/buildlogs.controller.js
@@ -1,18 +1,31 @@
 var app = angular.module('buildlogs.controller', []);
 app.controller('BuildLogsController', function BuildLogsController($routeParams, $location, BuildLog, ActiveProjectFactory) {
     this.basePath = $location.path();
+    this.buildlogs = [];
 
-    BuildLog.query({
-            username: $routeParams.username,
-            project_id: $routeParams.project_id
-    }).$promise
-        .then(angular.bind(this, function then(data) {
-            this.buildlogs = data.results;
-            this.numBuildlogs = data.count;
+    var currentPage = 1;
+    var loadBuildLogs = angular.bind(this, function(page_number) {
+        BuildLog.query({
+                username: $routeParams.username,
+                project_id: $routeParams.project_id,
+                page: page_number
+        }).$promise
+            .then(angular.bind(this, function then(data) {
+                this.buildlogs = this.buildlogs.concat(data.results);
+                this.numBuildlogs = data.count;
+                this.hasMore = this.buildlogs.length < this.numBuildlogs;
 
-            ActiveProjectFactory.getProject()
-                .then(angular.bind(this, function then(data) {
-                    this.project = data;
-                }));
-        }));
+                ActiveProjectFactory.getProject()
+                    .then(angular.bind(this, function then(data) {
+                        this.project = data;
+                    }));
+            }));
+    });
+
+    this.loadMore = function () {
+        currentPage += 1;
+        loadBuildLogs(currentPage);
+    };
+
+    loadBuildLogs(currentPage);
 });
