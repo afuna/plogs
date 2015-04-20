bee1f2aeb63946010e2fb6077a21892ff0c5fcff
Athena Yao <athena@fastmail.fm>
2015-04-19 23:50:56 -0400

    Check whether someone is logged in or out
    Quick check, mostly so we know whether to show the "sign up" blurb or
    the "your stats" section in the front page. Pretty rudimentary, but
    since it's in a service should not be completely terrible to modify later

--- plogs/main/views.py
+++ plogs/main/views.py
@@ -8,12 +8,7 @@ from plogs.buildlogs.models import Project, BuildLog
 
 
 def index(request):
-    if request.user.is_authenticated():
-        project = Project.objects.latest_for_user(request.user)
-        stats = BuildLog.statistics.for_project(project)
-        return render(request, 'buildlogs/frontpage.html', { "project_name": project.plane.kit.model, "stats": stats })
-    else:
-        return render(request, 'main/index.html')
+    return render(request, 'main/index.html')
 
 def signup(request):
     if request.method == 'POST':
