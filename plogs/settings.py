63a9565ea744ab198f027e8ad102cda7dcfe2bf3
Athena Yao <athena@fastmail.fm>
2015-05-14 19:18:03 -0700

    Log errors to console, instead of trying to email them

--- plogs/settings.py
+++ plogs/settings.py
@@ -114,6 +114,19 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20
 }
 
-# Errors
-if os.environ.get('ADMIN_EMAIL'):
-    ADMINS = (('Admin', os.environ.get('ADMIN_EMAIL')))
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
