c1f8792317ce9d85e6025006172932b70bef017c
Athena Yao <athena@fastmail.fm>
2015-05-15 20:50:27 -0700

    Add optional argument for cachebusting
    Only useful for dev work. Need a better strategy for site users (either
    lastmodtime or md5 of file contents should work). But that probably
    requires setting up some kind of preprocessor. This works for now.

--- plogs/main/src/js/utils.js
+++ plogs/main/src/js/utils.js
@@ -3,9 +3,13 @@
 
     angular.module('plogsUtils', [])
         .constant('assetsRoot', '/static/assets')
-        .factory('assets', function (assetsRoot) {
+        .factory('assets', function (assetsRoot, $location) {
             return function (path) {
-                return assetsRoot + "/" + path;
+                var cachebuster = "";
+                if ($location.search()['debug']) {
+                    cachebuster = "?v=" + (new Date()).getTime();
+                }
+                return assetsRoot + "/" + path + cachebuster;
             };
         });
 })();
