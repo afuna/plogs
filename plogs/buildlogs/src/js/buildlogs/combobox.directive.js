06bfea3bd43767fa958d95236a40e7c191f994fc
Athena Yao <athena@fastmail.fm>
2015-05-21 22:06:39 -0700

    Create new category/partner using TAB
    (technically it's created when we post the buildlog, but this allows the
    values to be retained by the frontend until then)

--- plogs/buildlogs/src/js/buildlogs/combobox.directive.js
+++ plogs/buildlogs/src/js/buildlogs/combobox.directive.js
@@ -15,10 +15,22 @@ app.directive('combobox', function () {
     return {
         require: '^uiSelect',
         link: function(scope, element, attrs, selectController) {
+            var select = selectController.select;
+            selectController.select = function(item, skipFocusser, $event) {
+                // we want to handle new items ourselves, not using the default
+                // (especially for TAB)
+                if (item === undefined) return;
+                select(item, skipFocusser, $event);
+            }
+
             selectController.searchInput.on("keydown", function (e) {
                 if (e.which == 13) {
                     e.preventDefault();
                     e.stopPropagation();
+                } else if (e.which == 9) {
+                    if (selectController.activeIndex == -1) {
+                        selectController.activate(false, true);
+                    }
                 }
             });
         }
