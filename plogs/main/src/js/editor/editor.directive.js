4bd9b7af22487bc4861c5b06d1df047a10507196
Athena Yao <athena@fastmail.fm>
2016-06-26 22:55:15 -0700

    Loop over all images and send off HTTP request immediately
    Instead of waiting for all conversion to complete -> sending all through
    s3upload
    
    No sense in this; we end up doing one HTTP request per file anyway (and somehow we
    end up with a race condition(?) which makes all the image ids the same)

--- plogs/main/src/js/editor/editor.directive.js
+++ plogs/main/src/js/editor/editor.directive.js
@@ -99,71 +99,68 @@ app.directive('editor', function (editorModuleAssets, $routeParams, $timeout) {
                 return "![Uploading " + filename + "]()\n";
             }
 
-            function fixImageOrientation(images, formData) {
-                var deferreds = [];
-                for (var i = 0; i < images.length; i++) {
-                    var deferred = new $.Deferred();
-                    deferreds.push(deferred);
-
-                    var image = images[i];
-                    loadImage.parseMetaData(
-                        image,
-                        function(deferred, image) {
-                            return function(data) {
-                                // get orientation from exif, or assume normal
-                                var orientation = data.exif ? data.exif.get('Orientation') : 1;
-                                loadImage(image,
-                                    function(canvas) {
-                                        canvas.toBlob(function(blob) {
-                                            formData.append('images', blob, image.name);
-                                            deferred.resolve();
-                                        },
-                                        image.type
-                                        );
-                                    },
-                                    { orientation: orientation, canvas: true });
-                            }
-                        }(deferred, image)
-                    );
-                }
-                return deferreds;
+            function fixImageOrientation(image) {
+                var deferred = new $.Deferred();
+                loadImage.parseMetaData(
+                    image,
+                    function(data) {
+                        // get orientation from exif, or assume normal
+                        var orientation = data.exif ? data.exif.get('Orientation') : 1;
+                        loadImage(image,
+                            function(canvas) {
+                                canvas.toBlob(function(blob) {
+                                    var formData = new FormData();
+                                    formData.append('image', blob, image.name);
+                                    deferred.resolveWith(this, [formData]);
+                                },
+                                image.type
+                                );
+                            },
+                            { orientation: orientation, canvas: true });
+                    }
+                );
+                return deferred;
             }
 
-            function s3_upload(e){
-                var formData = new FormData();
-                var fixedImagesDeferreds = fixImageOrientation(e.target.files || e.originalEvent.dataTransfer.files, formData);
-                $.when.apply($, fixedImagesDeferreds).then(function() {
-                    // after fixing all images, we upload them all to s3
-                    var s3upload = new S3Upload({
-                        files: formData.getAll('images'),
-                        s3_sign_put_url: '/people/' + $routeParams.username + '/build/photo_upload_url/',
-                        project: $routeParams.project,
-                        log_id: $routeParams.log_id,
-                        onProgress: function(percent, message, filename) {
-                        },
-                        onStart: function(filename) {
-                            codemirror.replaceSelection(upload_placeholder_text(filename));
-                        },
-                        onFinishS3Put: function(imageData, filename) {
-                            var placeholder_text = upload_placeholder_text(filename);
-                            var index = codemirror.getValue().indexOf(placeholder_text);
+            function uploadToS3(formData) {
+                // after fixing all images, we upload them all to s3
+                var s3upload = new S3Upload({
+                    files: [formData.get('image')],
+                    s3_sign_put_url: '/people/' + $routeParams.username + '/build/photo_upload_url/',
+                    project: $routeParams.project,
+                    log_id: $routeParams.log_id,
+                    onProgress: function(percent, message, filename) {
+                    },
+                    onStart: function(filename) {
+                        codemirror.replaceSelection(upload_placeholder_text(filename));
+                    },
+                    onFinishS3Put: function(imageData, filename) {
+                        var placeholder_text = upload_placeholder_text(filename);
+                        var index = codemirror.getValue().indexOf(placeholder_text);
 
-                            var pos_start = codemirror.posFromIndex(index);
-                            var pos_end = codemirror.posFromIndex(index + placeholder_text.length);
+                        var pos_start = codemirror.posFromIndex(index);
+                        var pos_end = codemirror.posFromIndex(index + placeholder_text.length);
 
-                            codemirror.replaceRange('![' + filename + '](' +  imageData.url +' "caption")\n',
-                                                    pos_start, pos_end);
+                        codemirror.replaceRange('![' + filename + '](' +  imageData.url +' "caption")\n',
+                                                pos_start, pos_end);
 
-                            var onUpload = scope.$eval(attrs.onUpload);
-                            scope.$apply(onUpload(imageData));
-                        }
-                    });
+                        var onUpload = scope.$eval(attrs.onUpload);
+                        scope.$apply(onUpload(imageData));
+                    }
                 });
             }
 
-            $("#image_upload").change(s3_upload);
+            function onFileReceive(e) {
+                var images = e.target.files || e.originalEvent.dataTransfer.files;
+                for (var i = 0; i < images.length; i++) {
+                    var imageFile = images[i];
+                    fixImageOrientation(imageFile).then(uploadToS3);
+                }
+            }
+
+            $("#image_upload").change(onFileReceive);
             $("body")
-                .on("drop", s3_upload)
+                .on("drop", onFileReceive)
                 .on("drop dragover dragleave", function(e) {
                     e.preventDefault();
                     e.stopPropagation();
